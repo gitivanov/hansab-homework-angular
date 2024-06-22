a338443d3c45ff24427808da1998f6cd5d11bf26
valeri <701128@gmail.com>
2024-06-22 19:34:13 +0300

    temporary switch off test from build automation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'http://localhost:8080/api' // Should be repalced to real PROD url (it also used for build automation in GitHub)
   };
   
