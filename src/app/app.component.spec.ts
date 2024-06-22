37d475424ccb5ea6ae925451af4008608c4036e4
valeri <701128@gmail.com>
2024-06-22 17:50:09 +0300

    fixed component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hansab-homework-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('User and Car Management');
   });
 });
